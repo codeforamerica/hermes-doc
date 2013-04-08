b7134f85ed17f9967d3b8abbd5b8f6b3ccf21978
Marcin Wichary <mwichary@gmail.com>
2013-04-08 11:22:20 -0700

    Adding missing files.

---
+++ ux/mocks/data.js
@@ -0,0 +1,36 @@
+var MOCKS = {
+  title: 'Hermes UX',
+
+  categories: [
+    {
+      title: 'Text messages',
+
+      mocks: [
+        { title: 'v1', url: 'mocks/sites/1-v1.html' },
+        { title: 'v2', url: 'mocks/sites/1-v2.html' },
+        { title: 'Future interaction', url: 'mocks/sites/1-interaction.html' },
+        { title: 'Future court date change', url: 'mocks/sites/1-court-date-changed.html' }
+      ]
+    },
+    {
+      title: 'Site',
+
+      mocks: [
+        { title: 'Two weeks before', url: 'mocks/images/1-site-homepage-two-weeks.png' },
+        { title: 'One day before', url: 'mocks/images/1-site-homepage-day-before.png' },
+        { title: 'One day after', url: 'mocks/images/1-site-homepage-day-after.png' },
+        { title: 'How do I get there?', url: 'mocks/images/1-site-how-do-i-get-there.png' },
+        { title: 'What if I don’t show up?', url: 'mocks/images/1-site-what-if-i-dont-show-up.png' }
+      ]
+    },
+    {
+      title: 'Dashboard',
+
+      mocks: [
+        { title: 'Homepage (Today)', url: 'mocks/images/1-dashboard-homepage-today.png' },
+        { title: 'Homepage (All time)', url: 'mocks/images/1-dashboard-homepage-all-time.png' },
+        { title: 'Specific case', url: 'mocks/images/1-dashboard-specific.png' }
+      ]
+    }
+  ]
+};
---
+++ ux/scripts.js
@@ -0,0 +1,112 @@
+var currentUrl = '';
+
+function updateHash() {
+  window.history.replaceState(null, '', '#' + currentUrl);
+}
+
+function onTabClickEl(el) {
+  if (currentUrl) {
+    document.querySelector('#content > div[href="' + currentUrl + '"]').
+        classList.remove('selected');
+    document.querySelector('nav > a[href="' + currentUrl + '"]').
+        classList.remove('selected');
+  }
+  
+  currentUrl = el.getAttribute('href');
+
+  document.querySelector('#content > div[href="' + currentUrl + '"]').
+      classList.add('selected');
+  document.querySelector('nav > a[href="' + currentUrl + '"]').
+      classList.add('selected');
+
+  updateHash();
+}
+
+function onTabClick(event) {
+  var el = event.target;
+  onTabClickEl(el);
+    
+  event.preventDefault();
+}
+
+function createNav() {
+  var titleEl = document.createElement('h1');
+  titleEl.innerHTML = MOCKS.title;
+  document.querySelector('nav').appendChild(titleEl);
+
+  document.title = MOCKS.title;
+
+  for (var i in MOCKS.categories) {
+    var category = MOCKS.categories[i];
+
+    var titleEl = document.createElement('h2');
+    titleEl.innerHTML = category.title;
+    document.querySelector('nav').appendChild(titleEl);
+
+    for (var j in category.mocks) {
+      var mockEl = document.createElement('a');
+      mockEl.href = category.mocks[j].url;
+      mockEl.innerHTML = category.mocks[j].title;
+      document.querySelector('nav').appendChild(mockEl);
+    }
+  }
+}
+
+function createMocks() {
+  var els = document.querySelectorAll('nav > a');
+  for (var i = 0, el; el = els[i]; i++) {
+    var url = el.getAttribute('href');
+
+    var parentEl = document.createElement('div');
+    parentEl.setAttribute('href', url);
+
+    if (url.substr(url.length - 5, 5) == '.html') {
+      var mockEl = document.createElement('iframe');
+    } else {
+      var mockEl = document.createElement('img');
+    }
+    mockEl.src = url;
+
+    parentEl.appendChild(mockEl);
+
+    document.querySelector('#content').appendChild(parentEl);
+  }
+}
+
+function addEventListeners() {
+  var els = document.querySelectorAll('nav > a');
+  for (var i = 0, el; el = els[i]; i++) {
+    el.addEventListener('click', onTabClick, false);
+  }
+
+  window.addEventListener('popstate', onPopState, false);
+}
+
+function determineStartingMock() {
+  var url = location.hash.substr(1);
+
+  var el = document.querySelector('nav > a[href="' + url + '"]');
+
+  if (el) {
+    onTabClickEl(el); 
+  } else {
+    onTabClickEl(document.querySelector('nav > a')); 
+  }
+}
+
+function onPopState(event) {
+  var url = location.hash.substr(1);
+
+  var el = document.querySelector('nav > a[href="' + url + '"]');
+
+  if (el) {
+    onTabClickEl(el); 
+  }
+}
+
+function main() {
+  createNav();
+  createMocks();
+  addEventListeners();
+  determineStartingMock();
+}
