82cdeac2057ca4ad0eaa484055ccb52590c1bb84
Marcin Wichary <mwichary@gmail.com>
2013-04-16 17:16:28 -0700

    Simulator: Moar (timestamps etc.).

--- simulator/js/data.js
+++ simulator/js/data.js
@@ -1,6 +1,9 @@
 var CASES = {
   '13-F-0102292': {
     defendantName: 'Kenisha Woods'
+  },
+  '13-M-0012391': {
+    defendantName: 'Markell Blackburn'
   }
 };
 
@@ -8,11 +11,12 @@ var STATES = {
 
   // Initial state, ready for user input
   'ready': {
-    cheatText: "<p>Imagine you’re seeing this poster, and holding your phone in your hand. What would you do?</p><img src='../ux/mocks/physical/business-card.png'><p>Example case numbers: 13-F-0102292</p>",
+    cheatText: "<p>Imagine you’re seeing this poster, and holding your phone in your hand. What would you do?</p><img src='../ux/mocks/physical/business-card.png'><p>Example case numbers:</p><ul><li>13-F-0102292<li>13-M-0012391</ul>",
 
     onEntry: function() {
-      //sendReply('Thank you. You need to come to court on {{courtDate}}, at {{courtTime}}. We will send you a reminder text message a day before your court date.');
-      //sendReply('{{clerkPhone}}');
+      sendReply('Thank you. You need to come to court on {{courtDate}}, at {{courtTime}}. We will send you a reminder text message a day before your court date.');
+      advanceTime('tomorrow');
+      sendReply('{{clerkPhone}}');
     },
     onTextMessage: function(input) {
       data.caseNumber = input;
@@ -63,6 +67,26 @@ var STATES = {
   'case-confirmed': {
     onEntry: function() {
       sendReply('Thank you. You need to come to court on {{courtDate}}, at {{courtTime}}. We will send you a reminder text message a day before your court date.');
+      changeState('waiting-for-reminders');
+    }
+  },
+
+  'waiting-for-reminders': {
+    cheatActions: [
+      { name: 'Fast forward to 1 day before', state: 'waiting-for-reminders-1-day-before' }
+    ],
+    onTextMessage: function() {
+      sendReply('You can’t text this number. Please call {{clerkPhone}} for more information about your court date.');
+    }
+  },
+
+  'waiting-for-reminders-1-day-before': {
+    onEntry: function() {
+      advanceTime('One day before the court date');
+      sendReply('Your court date is tomorrow: {{courtDate}}, at {{courtTime}}, in the Hall of Justice. If you can’t make it, call {{clerkPhone}} as soon as possible, today.')
+    },
+    onTextMessage: function() {
+      sendReply('You can’t text this number. Please call {{clerkPhone}} for more information about your court date.');
     }
   },
 
--- simulator/js/scripts.js
+++ simulator/js/scripts.js
@@ -18,11 +18,32 @@ function enterState(newState) {
   }
 
   if (STATES[state].cheatText) {
-    document.querySelector('#cheat-sheet').innerHTML = STATES[state].cheatText;
+    document.querySelector('#cheat-sheet').innerHTML += STATES[state].cheatText;
     document.querySelector('#cheat-sheet').classList.add('visible');
   } else {
     document.querySelector('#cheat-sheet').classList.remove('visible');
   }
+
+  if (STATES[state].cheatActions) {
+    if (!STATES[state].cheatText) {
+      document.querySelector('#cheat-sheet').innerHTML = '';      
+    }
+
+    for (var i in STATES[state].cheatActions) {
+      var el = document.createElement('button');
+      el.classList.add('cheat-action');
+      el.innerHTML = STATES[state].cheatActions[i].name;
+      el.setAttribute('state', STATES[state].cheatActions[i].state);
+      el.addEventListener('click', onCheatActionClick);
+
+      document.querySelector('#cheat-sheet').appendChild(el);
+    }
+    document.querySelector('#cheat-sheet').classList.add('visible');
+  }
+}
+
+function onCheatActionClick(event) {
+  changeState(event.target.getAttribute('state'));
 }
 
 function changeState(newState) {
@@ -46,6 +67,10 @@ function sendReply(text) {
   showReply(text);
 }
 
+function advanceTime(text) {
+  showTimestamp(text);
+}
+
 // visual
 
 function showReply(text) {
@@ -53,6 +78,16 @@ function showReply(text) {
 
   var el = document.createElement('div');
   el.classList.add('reply');
+  el.innerHTML = '<span>' + text + '</span>';
+
+  document.querySelector('#content').appendChild(el);
+  document.querySelector('#content').scrollTop = 99999999;
+}
+
+function showTimestamp(text) {
+
+  var el = document.createElement('div');
+  el.classList.add('timestamp');
   el.innerHTML = '<span>' + text + '</span>';
 
   document.querySelector('#content').appendChild(el);
